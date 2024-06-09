cdda1e8ed357116865f4ba7b62ec54cd51e26674
Milton Soares Filho <milton.soares.filho@gmail.com>
2024-06-09 23:43:58 +0200

    basic implementation
    I've tried a bunch of options here but then settled with an
    implementation using head and length references so the cost for a
    Count() operation would be minimal and the logic simpler.
    
    Signed-off-by: Milton Soares Filho <milton.soares.filho@gmail.com>

--- src/queue.h
+++ src/queue.h
@@ -8,59 +8,113 @@ template<class T>
 class Queue {
 public:
   Queue(int size);
+  virtual ~Queue();
   void Push(T element);
   T Pop();
   T PopWithTimeout(int milliseconds);
   int Count();
   int Size();
+
+private:
+  void advanceIndex(int &index);
+
+  int mSize;
+  int mHead, mLength;
+  T* mItems;
 };
 
-/** ctor, not much fun here */
+/** ctor, not much fun here
+ *
+ * although it's not a good idea mess with things that can fail inside the
+ * constructor such as dynamic allocation, I want to avoid the penalty of
+ * dealing with it during regular operations.
+ */
 template<class T>
-Queue<T>::Queue(int)
+Queue<T>::Queue(int size) :
+    mSize(size),
+    mHead(0), mLength(0),
+    mItems(nullptr)
 {
+  mItems = new T[size];
 }
 
-/** Pushes such @a element into the queue, respecting its limits */
+/** dtor */
+template<class T>
+Queue<T>::~Queue()
+{
+  delete[] mItems;
+}
+
+/** Boilerplate to advance the given index without trespassing queue size
+ *
+ * @param index  reference to index to be increased
+ */
+template<class T>
+void Queue<T>::advanceIndex(int &index)
+{
+  index = (index + 1) % mSize;
+}
+
+/** Pushes such @a element into the queue, respecting its limits
+ *
+ * Queues push elements at its end, let's use the array bounds to cycle it into
+ * the list's beginning. Advances head when needed.
+ */
 template<class T>
 void Queue<T>::Push(T element)
 {
+  mItems[(mHead + mLength) % mSize] = element;
+  if (mLength == mSize)
+    advanceIndex(mHead);
+
+  if (mLength < mSize)
+    mLength++;
 }
 
 /** Pops last element inserted at the queue
+*
+* @todo not handling blocking at the moment
 *
 * @return the last @a element, blocks calling thread if empty
 */
 template<class T>
 T Queue<T>::Pop()
 {
-  return T();
+  auto e = mItems[mHead];
+  if (mLength > 0)
+    mLength--;
+
+  advanceIndex(mHead);
+
+  return e;
 }
 
 /** Pops last element inserted at the queue
 *
 * @param milliseconds  timeout for blocking the calling thread if the queue is empty
 *
+* @todo handle timeout
+*
 * @return the last @a element or an invalid value if queue is empty
 */
 template<class T>
 T Queue<T>::PopWithTimeout(int milliseconds)
 {
-  return T();
+  return Pop();
 }
 
 /** Gives the current number of queued items */
 template<class T>
 int Queue<T>::Count()
 {
-  return 0;
+  return mLength;
 }
 
-/** Limit for the queued items */
+/** Limit capacity for the queued items */
 template<class T>
 int Queue<T>::Size()
 {
-  return 0;
+  return mSize;
 }
 
 #endif // _QUEUE_H
