2e8a2e9fb810628d41cfdfa249257da981919816
Milton Soares Filho <milton.soares.filho@gmail.com>
2024-06-09 11:36:22 +0200

    make it a header-only project
    It makes it easier to work with the templated class.
    
    Signed-off-by: Milton Soares Filho <milton.soares.filho@gmail.com>

--- src/queue.h
+++ src/queue.h
@@ -1,1 +1,49 @@
-// placeholder
+#ifndef _QUEUE_H
+#define _QUEUE_H 1
+
+template<class T>
+class Queue {
+public:
+  Queue(int size);
+  void Push(T element);
+  T Pop();
+  T PopWithTimeout(int milliseconds);
+  int Count();
+  int Size();
+};
+
+template<class T>
+Queue<T>::Queue(int)
+{
+}
+
+template<class T>
+void Queue<T>::Push(T element)
+{
+}
+
+template<class T>
+T Queue<T>::Pop()
+{
+  return T();
+}
+
+template<class T>
+T Queue<T>::PopWithTimeout(int milliseconds)
+{
+  return T();
+}
+
+template<class T>
+int Queue<T>::Count()
+{
+  return 0;
+}
+
+template<class T>
+int Queue<T>::Size()
+{
+  return 0;
+}
+
+#endif // _QUEUE_H
