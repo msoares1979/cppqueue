b2d0a2b1ed5f260383fcf75f657da73cdd738934
Milton Soares Filho <milton.soares.filho@gmail.com>
2024-06-09 18:48:32 +0200

    improve requirements via tests
    Signed-off-by: Milton Soares Filho <milton.soares.filho@gmail.com>

--- test/basic.cc
+++ test/basic.cc
@@ -1,6 +1,8 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
+#include <string>
+
 #include <queue.h>
 
 SCENARIO( "basic queue manipulation", "[basic]" ) {
@@ -28,6 +30,57 @@ SCENARIO( "basic queue manipulation", "[basic]" ) {
       THEN( "count is never bigger than size" ) {
         REQUIRE( q.Size() == size );
         REQUIRE( q.Count() == size );
+      }
+    }
+
+    WHEN( "a known value is pushed" ) {
+      const int known = 3;
+      q.Push(known);
+
+      THEN( "the same value is poped" ) {
+        REQUIRE( q.Pop() == known );
+      }
+
+      WHEN( "another value is pushed" ) {
+        const int another = 2;
+        q.Push(another);
+
+        THEN( "the previous value is poped" ) {
+          REQUIRE( q.Pop() == known );
+        }
+      }
+    }
+
+    WHEN( "a queue is full" ) {
+      for(int i = 0; i < size; i++)
+        q.Push(i);
+
+      REQUIRE( q.Count() == size );
+
+      THEN( "poping one item decreases the queue by one" ) {
+        auto item = q.Pop();
+        REQUIRE( q.Count() == (size - 1) );
+      }
+
+      THEN( "poping all items resets the queue" ) {
+        for(int i = 0; i < size; i++)
+          auto item = q.Pop();
+
+        REQUIRE( q.Count() == 0 );
+      }
+    }
+  }
+
+  // this is for testing multi datatype support
+  GIVEN( "a queue of strings with limited size" ) {
+    Queue<std::string> q(2);
+
+    WHEN( "a known string is pushed") {
+      const std::string fb = "foobar";
+      q.Push(fb);
+
+      THEN( "the same string is poped" ) {
+        REQUIRE( q.Pop() == fb );
       }
     }
   }
