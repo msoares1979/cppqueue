92accf87f11f0e7615914704b4dd4b4786f8270d
Milton Soares Filho <milton.soares.filho@gmail.com>
2024-06-09 12:24:26 +0200

    adopting BDD-style for testing
    It feels clearer to describe all the requisites.
    
    Signed-off-by: Milton Soares Filho <milton.soares.filho@gmail.com>

--- test/basic.cc
+++ test/basic.cc
@@ -3,7 +3,32 @@
 
 #include <queue.h>
 
-TEST_CASE("Basic Test", "[unit]")
-{
-	REQUIRE(1 == 0);
+SCENARIO( "basic queue manipulation", "[basic]" ) {
+
+  GIVEN( "a queue of integers with limited size" ) {
+    const int size = 2;
+    Queue<int> q(size);
+
+    REQUIRE( q.Size() == size );
+    REQUIRE( q.Count() == 0 );
+
+    WHEN( "an element is pushed" ) {
+      q.Push(1);
+
+      THEN( "only the count is modified" ) {
+        REQUIRE( q.Size() == size );
+        REQUIRE( q.Count() == 1 );
+      }
+    }
+
+    WHEN( "many elements are pushed" ) {
+      for(int i = 0; i < (2 * size); i++)
+        q.Push(i);
+
+      THEN( "count is never bigger than size" ) {
+        REQUIRE( q.Size() == size );
+        REQUIRE( q.Count() == size );
+      }
+    }
+  }
 }
